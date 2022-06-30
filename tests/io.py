65e1278846576627dcb174836b899e9ec2dcd3f6
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-06-30 18:28:32 +0200

    test output files

--- tests/io.py
+++ tests/io.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+from geocluster.io import write_output_file
 from tests.helpers import read_fixture_csv
 
 
@@ -7,3 +9,16 @@ def test_csv_filters():
     assert len(df) == 2
     assert df.iloc[0]["name"] == None
     assert df.iloc[1]["name"] == "Bob"
+
+
+def test_write_output_file():
+    p = "./this/dir/does/not/exist"
+    f = "test.txt"
+    write_output_file(p, f, "test")
+
+    path = Path(p) / f
+
+    with open(path) as f:
+        assert f.read() == "test"
+
+    path.unlink()
