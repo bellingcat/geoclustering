6a5cb3c3c34c6b483c9fe662685822e4c233a032
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-07-04 16:22:27 +0200

    feat: optional kepler.gl integration (#12)

--- setup.py
+++ setup.py
@@ -28,13 +28,13 @@ setup(
     install_requires=[
         "click",
         "geojson",
-        "keplergl",
         "numpy",
         "pandas",
         "scikit-learn",
     ],
     extras_require={
         "dev": ["black", "wheel", "pre-commit", "pytest"],
+        "full": ["keplergl"],
     },
     include_package_data=True,
     zip_safe=False,
